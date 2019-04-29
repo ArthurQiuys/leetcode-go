d9cb74b8bcf3df262b90eecd2ca71c37b164c137
qiuyuanshun <qiuyuanshun@yeah.net>
2019-04-30 00:15:24 +0800

    1

--- 104.Maximum Depth of Binary Tree/1.go
+++ 104.Maximum Depth of Binary Tree/1.go
@@ -2,7 +2,7 @@
 * @Author: qiuyu
 * @Date:   2019-04-29 23:15:18
 * @Last Modified by:   qiuyu
-* @Last Modified time: 2019-04-29 23:21:23
+* @Last Modified time: 2019-04-30 00:14:49
 *//**
  * /Definition for a binary tree node.
  */ 
@@ -24,7 +24,7 @@ func maxDepth(root *TreeNode) int {
     if root ==nil{
     	return 0
     }
-    return 1 + math.MaxInt32(maxDepth(root.Left),maxDepth(root.Right))
+    return 1 + Max(maxDepth(root.Left),maxDepth(root.Right))
 }
 func Max(a int, b int) int{
 	if a > b{
--- 78.Subsets/2.go
+++ 78.Subsets/2.go
@@ -2,7 +2,7 @@
 * @Author: qiuyu
 * @Date:   2019-04-29 19:21:24
 * @Last Modified by:   qiuyu
-* @Last Modified time: 2019-04-29 19:43:15
+* @Last Modified time: 2019-04-30 00:14:55
 */
 package main
 import (
@@ -18,7 +18,7 @@ func subsets(nums []int) [][]int {
     	 lens := len(res)
     	// var res []int
     	for j := 0; j < lens; j++ {
-    		temp := make([]int, len(res))
+    		temp := make([]int, len(res[j]))
     		copy(temp, res[j])
     		temp = append(temp, i)
     		res = append(res,temp)
