b5728cc372f63c976d8fbd5b5cafb7694b6796c2
qiuyuanshun <qiuyuanshun@yeah.net>
2019-04-30 00:16:15 +0800

    2

--- 14.Longest Common Prefix/1.go
+++ 14.Longest Common Prefix/1.go
@@ -2,5 +2,32 @@
 * @Author: qiuyu
 * @Date:   2019-04-26 03:19:45
 * @Last Modified by:   qiuyu
-* @Last Modified time: 2019-04-26 03:19:45
+* @Last Modified time: 2019-04-30 00:15:59
 */
+package main
+
+func main() {
+	
+}
+func longestCommonPrefix(strs []string) string {
+    if len(strs) == 0{
+        return ""
+    }
+    commonfix := strs[0]
+    for i:= 0 ;i < len(strs); i++{
+        if len(strs[i]) < len(commonfix){
+            commonfix = strs[i]
+            }
+    }
+    var prefix []byte
+    for i:= range commonfix{
+        for _, s:= range strs{
+            if s[i] != commonfix[i]{
+                return string(prefix)
+            }
+        }
+        prefix = append(prefix, commonfix[i])
+    }
+    return string(prefix)
+}
+// 4 ms
