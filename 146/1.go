24329e9b997c70111a128ad24755ef427a4dc617
qiuyuanshun <qiuyuanshun@yeah.net>
2019-05-03 21:07:34 +0800

    1

---
+++ 146/1.go
@@ -0,0 +1,30 @@
+/*
+* @Author: qiuyu
+* @Date:   2019-05-03 19:04:27
+* @Last Modified by:   qiuyu
+* @Last Modified time: 2019-05-03 21:07:25
+*/
+package main
+
+func main() {
+	
+}
+
+type LRUCache struct {
+    
+}
+
+
+func Constructor(capacity int) LRUCache {
+    
+}
+
+
+func (this *LRUCache) Get(key int) int {
+    
+}
+
+
+func (this *LRUCache) Put(key int, value int)  {
+    
+}
